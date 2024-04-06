427313ea979eabed0b034cf0acbc1894aa4b5c28
muthomi254 <dmunene202020@gmail.com>
2024-04-07 00:39:02 +0300

    (ft): creating basic info form and adding calender form

---
+++ src/app/(components)/BasicInfo.jsx
@@ -0,0 +1,143 @@
+// "use client"
+import React from 'react'
+import Calender from './Calender';
+
+export default function BasicInfo() {
+  return (
+    <div className="max-w-md mx-auto pb-10  pt-20 h-screen ">
+      {' '}
+      <form>
+        <div class="grid gap-6 mb-10  md:grid-cols-2">
+          <div>
+            <label
+              for="first_name"
+              class="block  mb-2 text-sm font-medium text-gray-900 "
+            >
+              First name
+            </label>
+            <input
+              type="text"
+              id="first_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder="John"
+              required
+            />
+          </div>
+          <div>
+            <label
+              for="last_name"
+              class="block mb-2  text-sm font-medium text-gray-900 "
+            >
+              Last name
+            </label>
+            <input
+              type="text"
+              id="last_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder="Doe"
+              required
+            />
+          </div>
+          <div>
+            <label
+              for="title"
+              class="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              Job Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder="Flowbite"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="dob"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Date of birth
+            </label>
+            <Calender />
+          </div>
+
+          <div>
+            <label
+              for="nationality"
+              class="block mb-2 text-sm font-medium text-gray-900 "
+              // class="relative z-0 w-full mb-5 group"
+            >
+              Nationality
+            </label>
+            <input
+              id="nationality"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder="Kenyan"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              for="Passport"
+              class="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              ID/Passport
+            </label>
+            <input
+              id="passport"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder="123-45-678"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="gender"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Gender
+            </label>
+            <select id="gender" name="gender" className="input-style">
+              <option value="">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Binary</option>
+            </select>
+          </div>
+        </div>
+        <div class="mb-6">
+          <label
+            class="block mb-2 text-sm font-medium text-gray-900 "
+            for="file_input"
+          >
+            Upload image
+          </label>
+
+          <input
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            aria-describedby="file_input_help"
+            id="file_input"
+            type="file"
+          />
+          <p
+            class="mt-1 text-sm text-gray-500 dark:text-gray-300"
+            id="file_input_help"
+          >
+            SVG, PNG, JPG or GIF (MAX. 800x400px).
+          </p>
+        </div>
+        
+        <button
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mb-10 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
