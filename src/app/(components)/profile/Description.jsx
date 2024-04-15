6a13637417cca80f97a66a99230c5173ffd513ab
muthomi254 <dmunene202020@gmail.com>
2024-04-15 10:36:40 +0300

    (ft):creating description form

--- src/app/(components)/profile/Description.jsx
+++ src/app/(components)/profile/Description.jsx
@@ -1,31 +1,31 @@
+
+
 import React from 'react';
 
 function Description() {
   return (
     <div>
-      <div className="max-w-md mx-auto pb-10  h-screen flex justify-center items-center">
+      <div className="max-w-md mx-auto pb-5 h-screen flex justify-center items-center">
         <form className="max-w-md w-full px-4">
-          {' '}
-          <div class="grid gap-6 mb-10  md:grid-cols-2">
+          <div className="grid gap-6 mb-10 md:grid-cols-2">
             <div>
               <label
-                for="  Description"
-                class="block  mb-2 text-sm font-medium text-gray-900 "
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Description
               </label>
-              <input
-                type="textarea"
-                id="  Description"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=""
-                required
-              />
+              <textarea
+                id="message"
+                rows="4"
+                className="block py-2.5 px-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 description-textarea"
+                placeholder="Tell Us More....."
+              ></textarea>
             </div>
           </div>
           <button
             type="save"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mb-10 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mb-10 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Save
           </button>
