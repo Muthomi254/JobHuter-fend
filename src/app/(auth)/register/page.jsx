f9ac141689269b287271509247dc8bfb3dbc7ce6
muthomi254 <dmunene202020@gmail.com>
2024-04-15 10:21:36 +0300

    (ft):adding and linking register to backend Form is functional

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,84 +1,196 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import { useAuth } from '../../(context)/authContext'; // Import the useAuth hook
+import Swal from 'sweetalert2'; // Import SweetAlert
 
 export default function Page() {
- 
- 
+  const { register } = useAuth();
+  const [error, setError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isSecurePassword = (password) => {
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const isLongEnough = password.length >= 8;
+
+    return (
+      hasUppercase &&
+      hasLowercase &&
+      hasNumber &&
+      hasSpecialChar &&
+      isLongEnough
+    );
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('floating_email').value;
+    const password = document.getElementById('password').value;
+    const confirm_password = document.getElementById('confirm_password').value;
+
+    if (password !== confirm_password) {
+      setError('Password confirmation does not match');
+      return;
+    }
+
+    if (!isSecurePassword(password)) {
+      setError(
+        'Password must contain at least 8 characters including uppercase, lowercase, number, and special character.'
+      );
+      return;
+    }
+
+    try {
+      await register({ email, password, confirm_password });
+      // If registration succeeds, show success message and redirect to login page
+      Swal.fire({
+        icon: 'success',
+        title: 'Registration Successful',
+        text: 'You have successfully registered!',
+      }).then(() => {
+        window.location.href = '/login';
+      });
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Registration Failed',
+        text: 'An error occurred while registering. Please try again later.',
+      });
+      setError('Registration failed');
+    }
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="max-w-md w-full px-4">
-        <div class="relative z-0 w-full mb-5 group">
-          <label for="floating_email" class="block mb-2 text-sm font-medium  ">
+      <form className="max-w-md w-full px-4" onSubmit={handleSubmit}>
+        <div className="relative z-0 w-full mb-5 group">
+          <label
+            htmlFor="floating_email"
+            className="block mb-2 text-sm font-medium  "
+          >
             Email address
           </label>
           <input
             type="email"
-            name="floating_email"
+            name="email"
             id="floating_email"
-            class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=" "
+            className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="email@example.com"
             required
           />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-grey-900 "
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-grey-900 "
           >
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="•••••••••"
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              name="password"
+              className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="•••••••••"
+              required
+            />
+            {/* Button to toggle password visibility */}
+            <button
+              type="button"
+              className="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-400 hover:text-gray-500 focus:outline-none"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M7 12a2 2 0 113.999-.001A2 2 0 017 12z"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            for="confirm_password"
-            class="block mb-2 text-sm font-medium text-grey-900 "
+            htmlFor="confirm_password"
+            className="block mb-2 text-sm font-medium text-grey-900 "
           >
             Confirm password
           </label>
           <input
             type="password"
             id="confirm_password"
-            class="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            name="confirm_password"
+            className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="•••••••••"
             required
           />
         </div>
-        <div class="flex items-start mb-5 text-sm font-small text-grey-900">
+        {error && <div className="text-red-500">{error}</div>}
+        <div className="flex items-start mb-5 text-sm font-small text-grey-900">
           Already have an account?
           <a
             href="/login"
-            class="text-blue-600 hover:text-blue-700 dark:text-blue-500 dark:hover:text-blue-600 font-medium  hover:underline ml-2"
+            className="text-blue-600 hover:text-blue-700 dark:text-blue-500 dark:hover:text-blue-600 font-medium  hover:underline ml-2"
           >
             Login
           </a>
         </div>
 
-        <div class="flex items-start mb-6">
-          <div class="flex items-center h-5">
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
             <input
               id="remember"
               type="checkbox"
               value=""
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
               required
             />
           </div>
 
           <label
-            for="remember"
-            class="ms-2 text-sm font-medium text-grey-900 dark:text-gray-300"
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-grey-900 dark:text-gray-300"
           >
             I agree with the{' '}
             <a
               href="#"
-              class="text-blue-600 hover:underline dark:text-blue-500"
+              className="text-blue-600 hover:underline dark:text-blue-500"
             >
               terms and conditions
             </a>
@@ -87,7 +199,7 @@ export default function Page() {
         </div>
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Register new account
         </button>
---
+++ src/app/(context)/authContext.jsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+import axios from 'axios';
+
+
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+ const register = async (formData) => {
+   try {
+     const response = await axios.post(`${BASE_URL}/register`, formData, {
+       headers: {
+         'Content-Type': 'application/json',
+       },
+     });
+     console.log('Request data:', formData); // Log request data
+     console.log('Response data:', response.data); // Log response data
+     return response.data;
+   } catch (error) {
+     console.error('Registration error:', error); // Log registration error
+     throw new Error('Registration failed');
+   }
+ };
+
+
+  const login = async (formData) => {
+    try {
+      const response = await axios.post('/api/login', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setUser(response.data.user);
+      localStorage.setItem('token', response.data.access_token);
+    } catch (error) {
+      console.error('Login error:', 'Login Failed');
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post('/api/logout', null, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      setUser(null);
+      localStorage.removeItem('token');
+    } catch (error) {
+      console.error('Logout error:', 'Logout Failed');
+    }
+  };
+
+  const forgotPassword = async (formData) => {
+    try {
+      const response = await axios.post('/api/forgot-password', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Forgot password error:', 'Forgot-password Failed');
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, register, login, logout, forgotPassword }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
