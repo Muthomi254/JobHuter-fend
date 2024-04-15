4fb6d20bac2929c5b2ff95bf602f894211628593
muthomi254 <dmunene202020@gmail.com>
2024-04-15 10:22:17 +0300

    (ft):creating calender for age

--- src/app/(components)/Calender.jsx
+++ src/app/(components)/Calender.jsx
@@ -14,9 +14,10 @@ function Calender() {
     <div>
       <DatePicker
         selected={selected}
-        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-blue-500 appearance-none dark:text-gray dark:border-blue-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        type="date"
+        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
         onChange={(date) => setSelected(date)}
-        placeholderText="Select Date of Birth"
+        placeholderText="Select a date"
         dateFormat="dd/MM/yyyy"
         showYearDropdown
         scrollableYearDropdown
