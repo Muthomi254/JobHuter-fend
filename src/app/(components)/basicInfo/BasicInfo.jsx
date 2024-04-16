b2cf5418a653e6771ee4c2e07064b8f4970150eb
muthomi254 <dmunene202020@gmail.com>
2024-04-16 14:16:49 +0300

    (ft):Enabling container to fetch data from the backend

--- src/app/(components)/basicInfo/BasicInfo.jsx
+++ src/app/(components)/basicInfo/BasicInfo.jsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BasicInfoForm from './BasicInfoForm';
 import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai';
+import { useBasicInfo } from '../../(context)/basicInfoContext'; // Import useBasicInfo hook
 
-const BasicInfoContainer = ({ recordedData }) => {
+const BasicInfoContainer = () => {
   const [showForm, setShowForm] = useState(false);
+  const { basicInfo, fetchBasicInfo } = useBasicInfo(); // Access basicInfo state and fetchBasicInfo function
 
-  const handleToggleForm = () => {
+  // Fetch basic info when component mounts
+  useEffect(() => {
+    fetchBasicInfo();
+  }, []); // Empty dependency array to trigger fetch only once on mount
+
+  const handleToggleForm = (event) => {
+    event.preventDefault(); // Prevent default behavior of the event
     setShowForm(!showForm);
   };
 
@@ -16,7 +24,49 @@ const BasicInfoContainer = ({ recordedData }) => {
       <h2 className="text-xl font-medium text-gray-900 mb-5">
         Personal Information
       </h2>
-      {!recordedData && !showForm && (
+      {basicInfo ? (
+        // Render basic info if available
+        <div className="bg-white shadow-md rounded-lg p-6">
+          {basicInfo.image_data && ( // Check if image data exists
+            <div className="mb-6 flex justify-center">
+              <img
+                src={`data:image/png;base64,${basicInfo.image_data}`}
+                alt="User"
+                className="w-32 h-32 rounded-full"
+              />
+            </div>
+          )}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6">
+            <div className="mb-6">
+              <p className="text-lg font-semibold mb-2">Name:</p>
+              <p className="text-gray-800">
+                {basicInfo.first_name} {basicInfo.last_name}
+              </p>
+            </div>
+            <div className="mb-6">
+              <p className="text-lg font-semibold mb-2">Job Title:</p>
+              <p className="text-gray-800">{basicInfo.job_title}</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-lg font-semibold mb-2">Date of Birth:</p>
+              <p className="text-gray-800">{basicInfo.date_of_birth}</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-lg font-semibold mb-2">Nationality:</p>
+              <p className="text-gray-800">{basicInfo.nationality}</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-lg font-semibold mb-2">Passport ID:</p>
+              <p className="text-gray-800">{basicInfo.passport_id}</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-lg font-semibold mb-2">Gender:</p>
+              <p className="text-gray-800">{basicInfo.gender}</p>
+            </div>
+          </div>
+        </div>
+        
+      ) : !showForm ? (
         <button
           onClick={handleToggleForm}
           className="text-blue-500 hover:text-blue-700 focus:outline-none"
@@ -26,8 +76,7 @@ const BasicInfoContainer = ({ recordedData }) => {
             Add Personal Info
           </div>
         </button>
-      )}
-      {showForm && (
+      ) : (
         <div>
           <button
             onClick={handleToggleForm}
@@ -35,10 +84,10 @@ const BasicInfoContainer = ({ recordedData }) => {
           >
             <div className="flex items-center">
               <AiOutlineClose className="h-5 w-5 mr-1" />
-              Close 
+              Close
             </div>
           </button>
-          <BasicInfoForm initialData={recordedData} />
+          <BasicInfoForm />
         </div>
       )}
     </div>
