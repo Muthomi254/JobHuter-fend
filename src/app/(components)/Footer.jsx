5e3d09504046ba51ba3e5b93753d1387bbd0657c
muthomi254 <dmunene202020@gmail.com>
2024-04-16 11:17:05 +0300

    (ft): Adding links to footer

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div class="sm:flex sm:items-center sm:justify-between">
             <a
-              href="https://flowbite.com/"
+              href="/Cv"
               class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
             >
               <img
@@ -16,12 +16,12 @@ function Footer() {
                 alt="Flowbite Logo"
               />
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                Job Hunter
               </span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
-                <a href="#" class="hover:underline me-4 md:me-6">
+                <a href="/about" class="hover:underline me-4 md:me-6">
                   About
                 </a>
               </li>
@@ -46,7 +46,7 @@ function Footer() {
           <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{' '}
             <a href="https://flowbite.com/" class="hover:underline">
-              Flowbite™
+              Job Hunter™
             </a>
             . All Rights Reserved.
           </span>
