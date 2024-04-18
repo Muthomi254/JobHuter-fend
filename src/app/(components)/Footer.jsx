24986aec41852330412f2227e1347c90a1fbcc07
muthomi254 <dmunene202020@gmail.com>
2024-04-18 20:45:56 +0300

    (ft): Adjusting footer styling

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -1,59 +1,61 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 function Footer() {
   return (
-    <div>
-      <footer class="bg-white  shadow dark:bg-gray-900 ">
-        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div class="sm:flex sm:items-center sm:justify-between">
-            <a
-              href="/Cv"
-              class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-            >
+    <footer className="bg-white shadow dark:bg-gray-900">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link href="/Cv">
+            <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
               <img
                 src="https://flowbite.com/docs/images/logo.svg"
-                class="h-8"
+                className="h-8"
                 alt="Flowbite Logo"
               />
-              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Job Hunter
               </span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-              <li>
-                <a href="/about" class="hover:underline me-4 md:me-6">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline me-4 md:me-6">
+            </div>
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <Link href="/about">
+                <div className="hover:underline me-4 md:me-6">About</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <div className="hover:underline me-4 md:me-6">
                   Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline me-4 md:me-6">
-                  Licensing
-                </a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{' '}
-            <a href="https://flowbite.com/" class="hover:underline">
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <div className="hover:underline me-4 md:me-6">Licensing</div>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <div className="hover:underline">Contact</div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
+        <div className="flex items-center justify-center flex-wrap">
+          <span className="inline-block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024
               Job Hunter™
-            </a>
+            
             . All Rights Reserved.
           </span>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 }
 
-export default Footer
+export default Footer;
