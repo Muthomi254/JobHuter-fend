ebc720ba6e965a5df447a434f261a75636642e86
muthomi254 <dmunene202020@gmail.com>
2024-04-18 15:46:08 +0300

    (ft):configuring get started to be logout upon log in

--- src/app/(components)/NavBar.jsx
+++ src/app/(components)/NavBar.jsx
@@ -1,62 +1,61 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Navbar, Button, Link } from 'react-flowbite';
-import { useAuth } from '../(context)/authContext'; // Import useAuth hook
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../(context)/authContext'; // Import useAuth hook
 
 function NavBar() {
-  const { user, logout } = useAuth(); // Destructure user and logout function from useAuth
+  const { user, logout } = useContext(AuthContext);
+  // Destructure user and logout function from useAuth
   const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu open/close
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track user login status
 
+  useEffect(() => {
+    if(user){
+      setIsLoggedIn(true)
+    }
+  }, [user])
   // Function to toggle menu state
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+
   return (
-    <Navbar
-      bg="white"
-      darkBg="gray-900"
-      fixed="top"
-      borderB="border-gray-200 dark:border-gray-600"
-    >
-      <Navbar.Container fluid>
+    <div className="pb-20">
+      <nav className="bg-white dark:bg-gray-900 fixed w-full z-50 top-0 border-b border-gray-200 dark:border-gray-600 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link
-            href="https://flowbite.com/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
+          <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-8"
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Job Hunter{' '}
             </span>
-          </Link>
+          </div>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             {/* Conditionally render "Logout" button if user is logged in, otherwise render "Get started" button */}
-            {user ? (
-              <Button
+            {isLoggedIn ? (
+              <button
                 onClick={logout} // Call logout function on button click
                 className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
               >
                 Logout
-              </Button>
+              </button>
             ) : (
-              <Link
+              <a
                 href="/register"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Get started
-              </Link>
+              </a>
             )}
-            <Button.NavbarToggle
+            <button
               onClick={toggleMenu} // Call toggleMenu function on button click
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              ariaControls="navbar-sticky"
-              ariaExpanded={isMenuOpen ? 'true' : 'false'} // Set aria-expanded attribute based on menu state
+              aria-controls="navbar-sticky"
+              aria-expanded={isMenuOpen ? 'true' : 'false'} // Set aria-expanded attribute based on menu state
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -74,50 +73,54 @@ function NavBar() {
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
-            </Button.NavbarToggle>
+            </button>
           </div>
 
-          <Navbar.Collapse id="navbar-sticky" isOpen={isMenuOpen}>
-            {/* Pass isOpen prop to collapse component */}
+          <div
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isMenuOpen ? 'block' : 'hidden'
+            }`}
+            id="navbar-sticky"
+          >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <Link
+                <a
                   href="/Cv"
                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                  ariaCurrent="page"
+                  aria-current="page"
                 >
                   Home
-                </Link>
+                </a>
               </li>
               <li>
-                <Link
+                <a
                   href="/about"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   About
-                </Link>
+                </a>
               </li>
               <li>
-                <Link
+                <a
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Services
-                </Link>
+                </a>
               </li>
               <li>
-                <Link
+                <a
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
-                </Link>
+                </a>
               </li>
             </ul>
-          </Navbar.Collapse>
+          </div>
         </div>
-      </Navbar.Container>
-    </Navbar>
+      </nav>
+    </div>
   );
 }
 
