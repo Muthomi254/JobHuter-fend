513ad0fe507f2670cd532aa3522ce0f7698916b5
muthomi254 <dmunene202020@gmail.com>
2024-04-18 20:47:06 +0300

    (chor):cleaning edit modal code

--- src/app/(components)/ui-components/EditModal.jsx
+++ src/app/(components)/ui-components/EditModal.jsx
@@ -1,36 +1,14 @@
 'use client';
 
-// Modal.js
-import React from 'react';
-import { useModal } from '../../(context)/modalContext';
-import { AiOutlineClose } from 'react-icons/ai';
+import { Button, Modal } from 'flowbite-react';
 
-const Modal = ({ buttonContent, children }) => {
-  const { showModal, openModal, closeModal } = useModal();
-
+export default function CustomModal({ title, size, children, open, onClose }) {
   return (
-    <>
-      <button
-        onClick={openModal}
-        className="text-blue-500 hover:text-blue-700 focus:outline-none flex items-center pt-10 relative z-10"
-      >
-        {buttonContent}
-      </button>
-      {showModal && (
-        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="p-8 rounded-lg bg-white max-w-md w-full max-h-screen overflow-y-auto relative z-20">
-            <button
-              onClick={closeModal}
-              className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-            >
-              <AiOutlineClose className="h-5 w-5" />
-            </button>
-            {children}
-          </div>
-        </div>
-      )}
-    </>
+    <div>
+      <Modal show={open} onClose={onClose} size={size} >
+        <Modal.Header>{title}</Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+      </Modal>
+    </div>
   );
-};
-
-export default Modal;
+}
