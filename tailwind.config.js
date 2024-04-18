c14477395488b01d8f5cb7e9b57abc1751ac061f
muthomi254 <dmunene202020@gmail.com>
2024-04-18 19:13:38 +0300

    (chor):

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const flowbite = require('flowbite-react/tailwind');
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,12 +8,11 @@ module.exports = {
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+
     flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
+  plugins: [flowbite.plugin()],
 };
