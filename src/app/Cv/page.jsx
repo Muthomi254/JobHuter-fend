c238ac00ec8dd3fda849854db846372b0f203913
muthomi254 <dmunene202020@gmail.com>
2024-05-04 01:16:54 +0300

    (ft):Adding templete1 and cvDisplay to cv page

--- src/app/Cv/page.jsx
+++ src/app/Cv/page.jsx
@@ -1,32 +1,22 @@
-// Page.js
 import React from 'react';
-import AuthenticatedPage from '../(context)/AuthenticatedPage'; // Import the AuthenticatedPage component
-import BasicInfo from '../(components)/basicInfo/BasicInfo';
-import Contact from '../(components)/contact/Contact';
-import Description from '../(components)/profile/Description';
-import Language from '../(components)/languages/Languages';
-import Education from '../(components)/education/Education';
-import Experience from '../(components)/experience/Experience';
-import Skills from '../(components)/skills/Skills';
-import References from '../(components)/references/References';
+import AuthenticatedPage from '../(context)/AuthenticatedPage';
+import CvDisplay from './CvDisplay';
+import Templete1 from '../(components)/templetes/Templete1';
 
 function Page() {
   return (
-      <div>
-       {/* <AuthenticatedPage> */}
-
-        <BasicInfo />
-        <Contact />
-        <Language />
-        <Education />
-        <Experience />
-        <Skills />
-        <References />
-        <Description />
-
-    {/* </AuthenticatedPage> */}
-
-      </div>
+    <div className="flex">
+      <AuthenticatedPage>
+        <div className="w-1/2">
+          <CvDisplay />
+        </div>
+      </AuthenticatedPage>
+      <AuthenticatedPage>
+        <div className="w-1/2">
+          <Templete1 />
+        </div>
+      </AuthenticatedPage>
+    </div>
   );
 }
 
