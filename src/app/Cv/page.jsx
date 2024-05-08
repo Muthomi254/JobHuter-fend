d52a0718af7284689f469e360082b3c9cf37429e
muthomi254 <dmunene202020@gmail.com>
2024-05-08 12:54:09 +0300

    (chor):Adjusting screen responsiveness on the page

--- src/app/Cv/page.jsx
+++ src/app/Cv/page.jsx
@@ -5,19 +5,17 @@ import Templete1 from '../(components)/templetes/Templete1';
 
 function Page() {
   return (
-          <AuthenticatedPage>
-
-    <div className="flex flex-row">
-        <div className="w-auto">
+    <AuthenticatedPage>
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-1/2">
           <CvDisplay />
         </div>
 
-        <div className="w-auto">
+        <div className="w-full lg:w-1/2">
           <Templete1 />
         </div>
-    </div>
-          </AuthenticatedPage>
-
+      </div>
+    </AuthenticatedPage>
   );
 }
 
