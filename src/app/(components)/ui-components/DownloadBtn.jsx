3161e81dec36c15dc734737e7de659c7e837e9c5
muthomi254 <dmunene202020@gmail.com>
2024-05-29 16:13:34 +0300

    (ft): Changing download logic

--- src/app/(components)/ui-components/DownloadBtn.jsx
+++ src/app/(components)/ui-components/DownloadBtn.jsx
@@ -3,31 +3,43 @@ import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 function DownloadButton({ content }) {
-  const handleDownload = () => {
+  const handleDownload = async () => {
     const input = document.getElementById(content);
+
+    // Get dimensions of the input element
     const inputWidth = input.offsetWidth;
     const inputHeight = input.offsetHeight;
 
-    html2canvas(input, {
-      scale: 2,
+    // Calculate aspect ratio of the input element
+    const aspectRatio = inputWidth / inputHeight;
+
+    // Define the size of the A4 paper
+    const a4Width = 210; // in mm
+    const a4Height = 297; // in mm
+
+    // Calculate the scale factor for best fit within A4 size
+    const widthScale = a4Width / inputWidth;
+    const heightScale = a4Height / inputHeight;
+
+    // Determine the scaling factor to maintain aspect ratio and fit within A4 size
+    let scale = Math.min(widthScale, heightScale);
+
+    // Create a new PDF document
+    const pdf = new jsPDF('p', 'mm', 'a4'); // 'p' for portrait orientation
+
+    // Capture the content as an image using html2canvas
+    const canvas = await html2canvas(input, {
+      scrollY: -window.scrollY,
       width: inputWidth,
       height: inputHeight,
-    }).then((canvas) => {
-      const imgData = canvas.toDataURL('image/jpeg', 1.0);
-      const pdf = new jsPDF('p', 'mm', 'a4'); // 'p' for portrait orientation
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-      const ratio = Math.min(pdfWidth / inputWidth, pdfHeight / inputHeight);
-      pdf.addImage(
-        imgData,
-        'JPEG',
-        0,
-        0,
-        inputWidth * ratio,
-        inputHeight * ratio
-      );
-      pdf.save('job_hunter_resume.pdf');
+      scale: 2, // scale factor for better quality
     });
+
+    // Add the captured image to the PDF with appropriate scaling
+    pdf.addImage(canvas, 'JPEG', 0, 0, inputWidth * scale, inputHeight * scale);
+
+    // Save the PDF
+    pdf.save('job_hunter_resume.pdf');
   };
 
   return (
